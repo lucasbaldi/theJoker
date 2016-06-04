3ea3d40cb19f4d07c86cf835edffb952c650d6d2
lucasbaldi <lucasbaldi@gmail.com>
2016-06-04 19:19:56 -0400

    updates to alexa and joke routes

--- theJoker/routes/alexa.js
+++ theJoker/routes/alexa.js
@@ -14,9 +14,8 @@ var headers = {
 };
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-
-
 
   var dataString = '{"text": "Hi Team, I know the times are difficult! Our sales have been disappointing for the past three quarters for our data analytics product suite. We have a competitive data analytics product suite in the industry. But we need to do our job selling it! "}';
 
@@ -34,8 +33,45 @@ router.get('/', function(req, res, next) {
       }
   }
 
+
   request.post(options, callback);
 });
+
+
+router.get('/joke', function(req, res, next) {
+  var joke;
+  request('http://52.23.184.18/jokes'
+    , function(err, response, body){
+      if(err){
+        console.log("err \n")
+        //console.log(err)
+        res.send(err)
+      }
+
+      console.log("body "+body)
+      joke=body;
+
+    }
+  )
+
+  var dataString = '{"text":'+ joke+'"}';
+
+  var options = {
+      url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19',
+      headers: headers,
+      body: dataString
+  };
+
+  function callback(error, response, body) {
+      if(error){
+        console.log(error)
+      }else{
+        res.send(response)
+      }
+  }
+});
+
+
 
 router.get('/:text', function(req, res, next) {
   var text = req.params.text;
